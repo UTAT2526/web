427598b1d227b99ddda89015894edb7d5451b94a
2646044472 <bankey2646044472@gmail.com>
2025-10-15 20:43:05 +0800

    1

---
+++ code/web/skin/js/index.js
@@ -0,0 +1,20 @@
+;(function () {
+	'use strict';
+	var sliderMain = function() {
+	
+	  	$('#fh5co-hero .flexslider').flexslider({
+			animation: "fade",
+			slideshowSpeed: 5000,
+			directionNav: true,
+			
+
+	  	});
+
+	};
+
+	$(function(){
+		sliderMain();
+	});
+
+
+}());
